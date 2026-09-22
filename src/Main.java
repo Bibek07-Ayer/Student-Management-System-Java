0d54c449523c0917621f75d7a58e8b879c4c678c
Bibek07-Ayer <ayerbibek222@gmail.com>
2026-09-22 16:33:01 +0545

    Added student DAO and collection operations

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,29 @@
+import dao.StudentDAO;
+import dao.StudentDAOImpl;
+import model.Student;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("=================================");
-        System.out.println("   STUDENT MANAGEMENT SYSTEM");
-        System.out.println("=================================");
+        StudentDAO studentDAO = new StudentDAOImpl();
 
+        Student student1 = new Student(
+                1,
+                "Bibek Ayer",
+                "bibek@gmail.com",
+                "9800000000",
+                "Kathmandu"
+        );
+
+        studentDAO.addStudent(student1);
+
+        System.out.println("Student added successfully!");
+
+        System.out.println("\nAll Students:");
+
+        for (Student student : studentDAO.getAllStudents()) {
+            student.displayInfo();
+        }
     }
 }
---
+++ src/dao/StudentDAOImpl.java
@@ -0,0 +1,57 @@
+package dao;
+
+import model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentDAOImpl implements StudentDAO {
+
+    private List<Student> students = new ArrayList<>();
+
+    @Override
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return students;
+    }
+
+    @Override
+    public Student getStudentById(int id) {
+
+        for (Student student : students) {
+
+            if (student.getId() == id) {
+                return student;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public void updateStudent(Student student) {
+
+        Student existingStudent = getStudentById(student.getId());
+
+        if (existingStudent != null) {
+            existingStudent.setName(student.getName());
+            existingStudent.setEmail(student.getEmail());
+            existingStudent.setPhone(student.getPhone());
+            existingStudent.setAddress(student.getAddress());
+        }
+    }
+
+    @Override
+    public void deleteStudent(int id) {
+
+        Student student = getStudentById(id);
+
+        if (student != null) {
+            students.remove(student);
+        }
+    }
+}
